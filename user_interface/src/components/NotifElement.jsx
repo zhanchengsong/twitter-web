83da26f1c50c328c39a9212c0fb18c858cde3c22
gordonli121 <gordonli121@gmail.com>
2020-09-05 20:08:26 -0400

    added notificatoin element jsx

---
+++ user_interface/src/components/NotifElement.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import { Container } from "react-bootstrap";
+
+export class NotifElement extends Component {
+  render() {
+    return (
+      <Container fluid className="d-block">
+        <p>look at me</p>
+      </Container>
+    );
+  }
+}
+
+export default NotifElement;
